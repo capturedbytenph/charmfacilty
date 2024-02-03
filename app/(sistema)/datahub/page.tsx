022106b05b4b6417bfc4ce94176905b66e71df61
Aldwin Ting <aldwin.ting@gmail.com>
2024-02-03 07:32:39 -0500

    datahub

--- app/(sistema)/datahub/page.tsx
+++ app/(sistema)/datahub/page.tsx
@@ -20,7 +20,7 @@ export default function Datahub() {
           TRANSACTIONS
         </CardTitle>
         <CardContent>
-          
+          LINE CHART KUNWARI
         </CardContent>
        
        </Card>
